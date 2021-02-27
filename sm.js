5411d94c57fa57df3064bddf7459f7e5d83dea17
Hyppoprogramm <65360617+Hyppoprogramm@users.noreply.github.com>
2021-02-27 17:20:21 +0500

    sm.js

--- sm.js
+++ sm.js
@@ -3,20 +3,24 @@
 // 27.02.2021
 
 let file = 'strings/strings.json';
+
 function reload(){
     let strs = document.getElementsByTagName('string');
+    let request = new XMLHttpRequest();
+    request.open('GET', 'http://www.yourwebsite.com/' + file, false); // Replace with your domain
+    request.send();
+    let json = JSON.parse(request.response);
     for (let i = 0; i < strs.length; i++) {
-        let request = new XMLHttpRequest();
-        request.open('GET', 'http://hippoapp.ultimatefreehost.in/' + file, true);
-        request.responseType = 'json';
-        request.send();
-        request.onload = function(){
-            strs[i].innerHTML = request.response[strs[i].id];
-        }
+        strs[i].innerHTML = json[strs[i].id];
     }
 }
-function setFile(f){
+function useFile(f){
     file = f;
     reload();
 }
+
+function setFile(f){
+    file = f;
+}
+
 document.addEventListener("DOMContentLoaded", reload);
