06220db9fb4527cb299c0b6e203d1c9822ec9fb4
Hyppoprogramm <65360617+Hyppoprogramm@users.noreply.github.com>
2021-02-27 13:03:19 +0500

    Create sm.js

---
+++ sm.js
@@ -0,0 +1,15 @@
+let file = 'strings.json';
+function reload(){
+  for(string in strings){
+    let fr = new FileReader(); 
+    fr.onload=function(){ 
+      let res = JSON.parse(fr.result);
+      string.innerHTML(res[string.getAttribute('id')]);
+    }
+    fr.readAsText(file); 
+  }
+}
+document.body.onload = function(){
+  let strings = document.GetElementsByTagName('string');
+  reload();
+}
